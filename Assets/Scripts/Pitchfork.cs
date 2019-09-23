932e2b8b2b9211c1d4c7678fa4a0c00f93c8ae56
BurningTurtle <moritz.unseld@googlemail.com>
2019-09-23 12:17:57 +0200

    Added pitchfork with functionality
    The farmer is now able to pick up a pitchfork and use it accordingly.

---
+++ Assets/Scripts/Pitchfork.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pitchfork : MonoBehaviour
+{
+
+    private bool taken;
+    private GameObject farmer;
+    private Rigidbody2D body;
+
+    private bool thrusting;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        body = GetComponent<Rigidbody2D>();
+        farmer = GameObject.Find("farmer");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (taken && !thrusting)
+        {
+            this.transform.position = farmer.transform.position + new Vector3(0.45f, -0.2f, 0);
+            rotateToMouse();
+            if (Input.GetMouseButtonDown(0))
+            {
+                StartCoroutine(thrust());
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject == farmer)
+        {
+            taken = true;
+        }
+    }
+
+    private void rotateToMouse()
+    {
+        
+        transform.rotation = Quaternion.Euler(new Vector3(0, 0, getAngle()));
+    }
+
+    private float getAngle()
+    {
+        Vector3 mousePos = Input.mousePosition;
+
+        Vector3 objectPos = Camera.main.WorldToScreenPoint(transform.position);
+        mousePos.x = mousePos.x - objectPos.x;
+        mousePos.y = mousePos.y - objectPos.y;
+
+        float angle = Mathf.Atan2(mousePos.y, mousePos.x) * Mathf.Rad2Deg;
+
+        return angle - 270;
+    }
+
+    IEnumerator thrust()
+    {
+        if (!thrusting)
+        {
+            thrusting = true;
+
+            Vector3 dir = Quaternion.AngleAxis(getAngle() - 90, Vector3.forward) * Vector3.right;
+            Debug.Log(dir);
+            body.AddForce(dir * 750);
+            yield return new WaitForSeconds(0.1f);
+            body.velocity = Vector3.zero;
+            thrusting = false;
+        }
+        
+    }
+}
